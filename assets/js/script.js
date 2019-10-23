2b58c642c53a04840b7db6c1e89b3b96c1ec5b9f
navidmx <navidmx@gmail.com>
2019-10-23 16:32:54 -0400

    Template for lesson

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Initialize JS libraries
+AOS.init();
+// BTG: Add particles here!
+
+// BTG: Add landing page fading here!
+
+// BTG: Fade in the navbar here!
+
+// BTG: Destroy the contact form here!
